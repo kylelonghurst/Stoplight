78d17d1402685b45401ed5eb75e2edc5b2fd285e
Austin Hewlett <aust.hewlett@gmail.com>
2014-11-22 14:12:01 -0700

    Basic Light Logic is in place for each button click. Needs to account for Light Changing Method, as well as implementing timers.

--- StopLight/StopLight/MainWindow.xaml.cs
+++ StopLight/StopLight/MainWindow.xaml.cs
@@ -33,18 +33,33 @@ namespace StopLight
         Stoplight Light1 = new Stoplight(false, false);
         Stoplight Light2 = new Stoplight(false, false);
         Stoplight Light3 = new Stoplight(false, false);
+        Stoplight Light3L = new Stoplight(false, false);
         Stoplight Light4 = new Stoplight(false, false);
         Stoplight Light4R = new Stoplight(false, false);
 
         public MainWindow()
         {
             InitializeComponent();
+            DefaultLogic();
 
             //iLeftRight = 0;
 
             //dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             //dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
             //dispatcherTimer.Start();
+        }
+
+        private void DefaultLogic()
+        {
+            // turn light 3 and 4R on
+            Light3.turnON(BottomRed,BottomYellow, BottomGreen);
+            Light3L.turnON(BottomRed, BottomLeftYellow, BottomLeftGreen);
+            Light4R.turnON(LeftRed, LeftRightYellow, LeftRightGreen);
+
+            // turn off lights 1, 2, and 4
+            Light1.turnOff(TopRed, TopYellow, TopGreen);
+            Light2.turnOff(RightRed, RightYellow, RightGreen);
+            Light4.turnOff(LeftRed, LeftYellow, LeftGreen); 
         }
 
         private void SleepFive()
@@ -59,111 +74,37 @@ namespace StopLight
 
         private void LightLogic1()
         {
+            // turn off lights 2, 3, and 4
+            Light2.turnOff(RightRed, RightYellow, RightGreen);
+            Light3.turnOff(BottomRed, BottomYellow, BottomGreen);
+            Light4.turnOff(LeftRed, LeftYellow, LeftGreen);
+            Light3L.turnOff(BottomRed, BottomLeftYellow, BottomLeftGreen);
+            Light4R.turnOff(LeftRed, LeftRightYellow, LeftRightGreen);
 
+            // turn light 3 and 4R on
+            Light1.turnON(TopRed, TopYellow, TopGreen);
         }
 
         private void LightLogic2and4()
         {
+            // turn off lights 1 and 3
+            Light1.turnOff(TopRed, TopYellow, TopGreen);
+            Light3.turnOff(BottomRed, BottomYellow, BottomGreen);
+            Light3L.turnOff(BottomRed, BottomLeftYellow, BottomLeftGreen);
+            
 
+            // turn light 2, 4, and 4R
+            Light2.turnON(RightRed, RightYellow, RightGreen);
+            Light4.turnON(LeftRed, LeftYellow, LeftGreen);
+            Light4R.turnON(LeftRed, LeftRightYellow, LeftRightGreen);
+            
         }
 
         private void LightLogic3()
         {
-
+            DefaultLogic();
         }
-        /*
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            SolidColorBrush red = new SolidColorBrush(Colors.Red);
-            SolidColorBrush green = new SolidColorBrush(Colors.Green);
-            SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
-            SolidColorBrush gray = new SolidColorBrush(Colors.LightGray);
 
-                if (iLeftRight == 0)
-                {
-                    LeftGreen.Fill = gray;
-                    LeftYellow.Fill = gray;
-                    LeftRed.Fill = red;
-
-                    RightGreen.Fill = gray;
-                    RightYellow.Fill = gray;
-                    RightRed.Fill = red;
-
-                    TopGreen.Fill = green;
-                    TopYellow.Fill = gray;
-                    TopRed.Fill = gray; 
-
-                    BottomGreen.Fill = green;
-                    BottomYellow.Fill = gray;
-                    BottomRed.Fill = gray;
-
-                    iLeftRight = 1;
-                }
-
-                else if (iLeftRight == 1)
-                {
-                    LeftGreen.Fill = gray;
-                    LeftYellow.Fill = gray;
-                    LeftRed.Fill = red;
-
-                    RightGreen.Fill = gray;
-                    RightYellow.Fill = gray;
-                    RightRed.Fill = red;
-
-                    TopGreen.Fill = gray;
-                    TopYellow.Fill = yellow;
-                    TopRed.Fill = gray;
-
-                    BottomGreen.Fill = gray;
-                    BottomYellow.Fill = yellow;
-                    BottomRed.Fill = gray;
-
-                    iLeftRight = 2;
-                }
-
-                else if (iLeftRight == 2)
-                {
-                    LeftGreen.Fill = green;
-                    LeftYellow.Fill = gray;
-                    LeftRed.Fill = gray;
-
-                    RightGreen.Fill = green;
-                    RightYellow.Fill = gray;
-                    RightRed.Fill = gray;
-
-                    TopGreen.Fill = gray;
-                    TopYellow.Fill = gray;
-                    TopRed.Fill = red;
-
-                    BottomGreen.Fill = gray;
-                    BottomYellow.Fill = gray;
-                    BottomRed.Fill = red;
-
-                    iLeftRight = 3;
-                }
-
-                else if (iLeftRight == 3)
-                {
-                    LeftGreen.Fill = gray;
-                    LeftYellow.Fill = yellow;
-                    LeftRed.Fill = gray;
-
-                    RightGreen.Fill = gray;
-                    RightYellow.Fill = yellow;
-                    RightRed.Fill = gray;
-
-                    TopGreen.Fill = gray;
-                    TopYellow.Fill = gray;
-                    TopRed.Fill = red;
-
-                    BottomGreen.Fill = gray;
-                    BottomYellow.Fill = gray;
-                    BottomRed.Fill = red;
-
-                    iLeftRight = 0;
-                }
-            }
- */
 
         private void btnleft_Click(object sender, RoutedEventArgs e)
         {
@@ -176,24 +117,124 @@ namespace StopLight
         {
             Light3.atStoplight = true;
             Btn3.IsEnabled = false;
+            LightLogic3();
         }
-
+       
         private void Btn2Click(object sender, RoutedEventArgs e)
         {
             Light2.atStoplight = true;
             Btn2.IsEnabled = false;
+            LightLogic2and4();
         }
 
         private void Btn1Click(object sender, RoutedEventArgs e)
         {
             Light1.atStoplight = true;
             Btn1.IsEnabled = false;
+            LightLogic1();
+            
         }
 
         private void Btn4Click(object sender, RoutedEventArgs e)
         {
             Light4.atStoplight = true;
             Btn4.IsEnabled = false;
+            LightLogic2and4();
         }
     }
 }
+
+
+/*
+private void dispatcherTimer_Tick(object sender, EventArgs e)
+{
+    SolidColorBrush red = new SolidColorBrush(Colors.Red);
+    SolidColorBrush green = new SolidColorBrush(Colors.Green);
+    SolidColorBrush yellow = new SolidColorBrush(Colors.Yellow);
+    SolidColorBrush gray = new SolidColorBrush(Colors.LightGray);
+
+        if (iLeftRight == 0)
+        {
+            LeftGreen.Fill = gray;
+            LeftYellow.Fill = gray;
+            LeftRed.Fill = red;
+
+            RightGreen.Fill = gray;
+            RightYellow.Fill = gray;
+            RightRed.Fill = red;
+
+            TopGreen.Fill = green;
+            TopYellow.Fill = gray;
+            TopRed.Fill = gray; 
+
+            BottomGreen.Fill = green;
+            BottomYellow.Fill = gray;
+            BottomRed.Fill = gray;
+
+            iLeftRight = 1;
+        }
+
+        else if (iLeftRight == 1)
+        {
+            LeftGreen.Fill = gray;
+            LeftYellow.Fill = gray;
+            LeftRed.Fill = red;
+
+            RightGreen.Fill = gray;
+            RightYellow.Fill = gray;
+            RightRed.Fill = red;
+
+            TopGreen.Fill = gray;
+            TopYellow.Fill = yellow;
+            TopRed.Fill = gray;
+
+            BottomGreen.Fill = gray;
+            BottomYellow.Fill = yellow;
+            BottomRed.Fill = gray;
+
+            iLeftRight = 2;
+        }
+
+        else if (iLeftRight == 2)
+        {
+            LeftGreen.Fill = green;
+            LeftYellow.Fill = gray;
+            LeftRed.Fill = gray;
+
+            RightGreen.Fill = green;
+            RightYellow.Fill = gray;
+            RightRed.Fill = gray;
+
+            TopGreen.Fill = gray;
+            TopYellow.Fill = gray;
+            TopRed.Fill = red;
+
+            BottomGreen.Fill = gray;
+            BottomYellow.Fill = gray;
+            BottomRed.Fill = red;
+
+            iLeftRight = 3;
+        }
+
+        else if (iLeftRight == 3)
+        {
+            LeftGreen.Fill = gray;
+            LeftYellow.Fill = yellow;
+            LeftRed.Fill = gray;
+
+            RightGreen.Fill = gray;
+            RightYellow.Fill = yellow;
+            RightRed.Fill = gray;
+
+            TopGreen.Fill = gray;
+            TopYellow.Fill = gray;
+            TopRed.Fill = red;
+
+            BottomGreen.Fill = gray;
+            BottomYellow.Fill = gray;
+            BottomRed.Fill = red;
+
+            iLeftRight = 0;
+        }
+    }
+*/
